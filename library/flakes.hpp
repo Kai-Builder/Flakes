ec5bdec77f9f3799457f6e092e1c57491d33b111
seymo <gkai70264@gmail.com>
2021-03-04 09:44:42 -0500

    fix flakes.hpp

--- library/flakes.hpp
+++ library/flakes.hpp
@@ -202,3 +202,30 @@ namespace std {
 
 
 #endif //FLAKES_TIME_H
+//
+// Created by seymo on 3/4/2021.
+//
+
+#ifndef FLAKES_MAKE_H
+#define FLAKES_MAKE_H
+
+#include <fstream>
+
+namespace std {
+    namespace makefiles {
+        struct makefile {
+            std::string name;
+            std::string ext;
+        };
+        void makefileconcat(makefile& make) {
+            std::ifstream sfile(make.name + make.ext);
+            std::string foo;
+            std::string lineargs;
+            while (sfile >> foo) {
+                getline(sfile, lineargs, '\n');
+                system((foo += " " + lineargs).c_str());
+            }
+        }
+    }
+}
+#endif //FLAKES_MAKE_H
